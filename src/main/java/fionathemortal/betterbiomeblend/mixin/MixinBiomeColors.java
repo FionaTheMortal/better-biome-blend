d2fb6aa4ce4b02b33988dbf705a90474c5ea685f
FionaTheMortal <66226130+FionaTheMortal@users.noreply.github.com>
2021-02-19 17:31:56 +0100

    Moved all client side code to its own class that isn't loaded on dediated servers

--- src/main/java/fionathemortal/betterbiomeblend/mixin/MixinBiomeColors.java
+++ src/main/java/fionathemortal/betterbiomeblend/mixin/MixinBiomeColors.java
@@ -3,8 +3,7 @@ package fionathemortal.betterbiomeblend.mixin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 
-import fionathemortal.betterbiomeblend.BetterBiomeBlend;
-import fionathemortal.betterbiomeblend.BlendedColorChunk;
+import fionathemortal.betterbiomeblend.BetterBiomeBlendClient;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockDisplayReader;
 import net.minecraft.world.biome.BiomeColors;
@@ -16,7 +15,7 @@ public class MixinBiomeColors
 	public static int 
 	getWaterColor(IBlockDisplayReader worldIn, BlockPos blockPosIn) 
 	{
-		int result = BetterBiomeBlend.getWaterColor(worldIn, blockPosIn);
+		int result = BetterBiomeBlendClient.getWaterColor(worldIn, blockPosIn);
 		
 		return result;
 	}
@@ -25,7 +24,7 @@ public class MixinBiomeColors
 	public static int 
 	getGrassColor(IBlockDisplayReader worldIn, BlockPos blockPosIn) 
 	{
-		int result = BetterBiomeBlend.getGrassColor(worldIn, blockPosIn);
+		int result = BetterBiomeBlendClient.getGrassColor(worldIn, blockPosIn);
 		
 		return result;
 	}
@@ -34,7 +33,7 @@ public class MixinBiomeColors
 	public static int 
 	getFoliageColor(IBlockDisplayReader worldIn, BlockPos blockPosIn) 
 	{
-		int result = BetterBiomeBlend.getFoliageColor(worldIn, blockPosIn);
+		int result = BetterBiomeBlendClient.getFoliageColor(worldIn, blockPosIn);
 		
 		return result;
 	}
