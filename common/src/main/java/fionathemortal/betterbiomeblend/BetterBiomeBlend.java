b1e2dd67eb7bce9d9a24ad44c36acfc1c65582b3
Fiona Tessmann <66226130+FionaTheMortal@users.noreply.github.com>
2026-04-13 21:15:56 +0200

    Strip sodium check

--- common/src/main/java/fionathemortal/betterbiomeblend/BetterBiomeBlend.java
+++ common/src/main/java/fionathemortal/betterbiomeblend/BetterBiomeBlend.java
@@ -3,22 +3,4 @@ package fionathemortal.betterbiomeblend;
 public class BetterBiomeBlend
 {
     public static final String MOD_ID = "betterbiomeblend";
-
-    public static boolean
-    isRunningSodium()
-    {
-        try
-        {
-            Class.forName(
-                "me.jellysquid.mods.sodium.client.world.biome.BlockColorCache",
-                false,
-                Thread.currentThread().getContextClassLoader());
-
-            return true;
-        }
-        catch (ClassNotFoundException e)
-        {
-            return false;
-        }
-    }
 }
