91bfa38052d21f2690d5cbb54ac9cfed8a7d1269
Fiona Tessmann <66226130+FionaTheMortal@users.noreply.github.com>
2026-04-13 20:41:29 +0200

    Strip debug cmd

--- fabric/src/main/java/fionathemortal/betterbiomeblend/fabric/BetterBiomeBlendFabric.java
+++ fabric/src/main/java/fionathemortal/betterbiomeblend/fabric/BetterBiomeBlendFabric.java
@@ -1,16 +1,16 @@
 package fionathemortal.betterbiomeblend.fabric;
 
-import fionathemortal.betterbiomeblend.BetterBiomeBlendClient;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 
 public class BetterBiomeBlendFabric implements ClientModInitializer
 {
     @Override
     public void onInitializeClient()
     {
+        /*
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
                 BetterBiomeBlendClient.registerCommands(dispatcher);
         });
+        */
     }
 }
--- forge/src/main/java/fionathemortal/betterbiomeblend/forge/BetterBiomeBlendForgeClient.java
+++ forge/src/main/java/fionathemortal/betterbiomeblend/forge/BetterBiomeBlendForgeClient.java
@@ -1,15 +1,5 @@
 package fionathemortal.betterbiomeblend.forge;
 
-import com.mojang.brigadier.CommandDispatcher;
-import com.mojang.brigadier.builder.LiteralArgumentBuilder;
-import fionathemortal.betterbiomeblend.BetterBiomeBlendClient;
-import fionathemortal.betterbiomeblend.common.debug.Debug;
-import fionathemortal.betterbiomeblend.common.debug.DebugSummary;
-import net.minecraft.client.Minecraft;
-import net.minecraft.commands.CommandSourceStack;
-import net.minecraft.commands.Commands;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
@@ -19,8 +9,10 @@ public class BetterBiomeBlendForgeClient
     public static void
     registerCommandsEvent(RegisterCommandsEvent event)
     {
+        /*
         CommandDispatcher<CommandSourceStack> dispatcher = event.getDispatcher();
 
         BetterBiomeBlendClient.registerCommands(dispatcher);
+        */
     }
 }
